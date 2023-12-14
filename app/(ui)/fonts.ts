0ee487318de72b18e368661779d5e8bbec570e5c
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-12-14 13:01:02 +0530

    settingUp the NextJsProject in the GitHub

---
+++ app/(ui)/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Whisper } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const whisper = Whisper({
+  weight: ['400'],
+  subsets: ['latin'],
+});
